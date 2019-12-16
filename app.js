8440b12acaa786b8484238f2effb4f2b8ec1792b
Shane Larson <shane@grizzlypeaksoftware.com>
2019-12-15 15:32:01 -0900

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,6 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+var https_server = http.createServer(server_config, app).listen(2600, function(err){
+    console.log("Node.js Express HTTPS Server Listening on Port 2600");
 });
